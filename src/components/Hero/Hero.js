456c90b0d7635ce9deba422dfec131d3327af21a
Ordan Gramatov <ordance01@yahoo.com>
2022-03-29 17:55:41 +0200

    navbar bug fixed

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,8 +6,8 @@ const Hero = () => {
     <div className='hero'>
       <Container>
         <img className='hero-image' src={require('../../Assets/images/rick-and-morty-header-image.png')}/>
-        <h1 className='hero-heading'>The Rick and Morty Library</h1>
-        <p className='hero-text'>Welcome to The Rick and Morty Library here you will informations about all the characters, <br/>locations and episodes from Rick and Morty.</p>
+        <h1 className='hero-heading'>The Rick & Morty Library</h1>
+        <p className='hero-text'>Here you will find all the characters, locations and episodes from the TV show Rick and Morty.</p>
       </Container>
     </div>
   )
